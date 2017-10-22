4313ea2ae64fdcc0efaa0ab0ac26e90030bc1749
tengkiatwong <tengkiatwong@gmail.com>
2017-10-21 19:48:37 -0700

    first page skeleton

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-impoty { location } from 'expo';
+import { location } from 'expo';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+  render() {
+    return (<View style={styles.container}>
+        <View>
+          <Text style={styles.headerText}>What are you looking for today?</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Sportswear</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Dining</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Groceries</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>Watches</Text>
+        </View>
+
+      </View>);
+  }
+}
+
+const AutoPilot = StackNavigator({
+  Home: { screen: HomeScreen }
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <AutoPilot />;
   }
 }
 
@@ -19,6 +44,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center'
   },
+  headerText: {
+    marginTop:40
+    //fontSize: 30
+    //color: blue
+  },
+  card: {
+    height:40,
+    flex: 1,
+    width: 360,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da'
+  }
 });
