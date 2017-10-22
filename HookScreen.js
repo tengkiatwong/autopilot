53552fc9efa8070cee0b5050e2363f757e814412
tessazglue <tessa@zglue.com>
2017-10-22 06:18:47 -0700

    fix cards in HoiokScreen

--- HookScreen.js
+++ HookScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
-
+import { Image } from 'react-native'
 const cards = [
   {
     text: 'Card One',
@@ -12,47 +12,28 @@ const cards = [
   },
 ];
 
+
+
 export default class HookScreen extends React.Component {
   render() {
     return (
-      <Container style={{marginHorizontal: 10}}> 
+      <Container style={{marginHorizontal: 10}}>
         <View >
           <DeckSwiper
             dataSource={cards}
             renderItem={item =>
               <Card style={{ elevation: 3 }}>
-
                 <CardItem cardBody>
-                  <Left>  
+                  <Left>
                     <Body>
                       <Text>Dining</Text>
-                      <Image style={{ height: 300, flex: 1 }} source={'./img/hook1'} />
+                      <Image style={{ height: 300, alignItems:'center' }} source={require('./img/hook1.png')} />
                       <Text note>Fish hook to catch amazing dining deals</Text>
                     </Body>
                   </Left>
                 </CardItem>
-
-                <CardItem cardBody>
-                  <Left>  
-                    <Body>
-                      <Text>Sportswear</Text>
-                      <Image style={{ height: 300, flex: 1 }} source={'./img/hook2'} />
-                      <Text note>Fish hook to catch amazing dining deals</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-
-                <CardItem cardBody>
-                  <Left>  
-                    <Body>
-                      <Text>Formal Apparel</Text>
-                      <Image style={{ height: 300, flex: 1 }} source={'./img/hook3'} />
-                      <Text note>Fish hook to catch amazing dining deals</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-
               </Card>
+
             }
           />
         </View>
